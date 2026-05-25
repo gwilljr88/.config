a2bc989ac0144da98c52b6ab88de080394903739
gwilljr88 <gwilljr88@gmail.com>
2026-05-25 12:18:10 -0500

    Remaped <leader>tx to :bdelete from :tabclose

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -119,7 +119,7 @@ vim.keymap.set("n", "<C-l>", ":wincmd l<CR>", opts)
 
 -- Tabs
 vim.keymap.set("n", "<leader>to", ":tabnew<CR>", opts) -- open new tab
-vim.keymap.set("n", "<leader>tx", ":tabclose<CR>", opts) -- close current tab
+vim.keymap.set("n", "<leader>tx", ":bdelete<CR>", opts) -- close current buffer
 vim.keymap.set("n", "<leader>tn", ":tabn<CR>", opts) --  go to next tab
 vim.keymap.set("n", "<leader>tp", ":tabp<CR>", opts) --  go to previous tab
 
